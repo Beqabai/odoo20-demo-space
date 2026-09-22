3da07e7512b176a61d4b4c61825eb36f6e571f93
Beqabai <122342551+Beqabai@users.noreply.github.com>
2026-09-22 20:09:24 +0400

    Update __manifest__.py

--- addons/web_responsive/__manifest__.py
+++ addons/web_responsive/__manifest__.py
@@ -9,7 +9,7 @@
 {
     "name": "Web Responsive",
     "summary": "Responsive web client, community-supported",
-    "version": "19.0.1.1.0",
+    "version": "20.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/web",
     "author": "LasLabs, Tecnativa, ITerra, Onestein, Odoo Community Association (OCA)",
